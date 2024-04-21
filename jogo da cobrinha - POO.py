3f14727595039a2a7361b7f4f56389cd27abf36f
kaua <kauavieirah2@gmail.com>
2024-04-20 23:22:20 -0300

    versão 2.0
    - degradê no corpo da cobra
    - maçã não spawna mais dentro do corpo
    - mapa menor
    - cobra maior
    - maçã maior
    - final adicionado

--- jogo da cobrinha - POO.py
+++ jogo da cobrinha - POO.py
@@ -27,13 +27,13 @@ class SNAKE:
 
     def mudar_direcao(self):
         if self.direcao == cima:
-            self.snake[0] = (self.snake[0][0], self.snake[0][1] - 20)
+            self.snake[0] = (self.snake[0][0], self.snake[0][1] - 60)
         if self.direcao == baixo:
-            self.snake[0] = (self.snake[0][0], self.snake[0][1] + 20)
+            self.snake[0] = (self.snake[0][0], self.snake[0][1] + 60)
         if self.direcao == direita:
-            self.snake[0] = (self.snake[0][0] + 20, self.snake[0][1])
+            self.snake[0] = (self.snake[0][0] + 60, self.snake[0][1])
         if self.direcao == esquerda:
-            self.snake[0] = (self.snake[0][0] - 20, self.snake[0][1])
+            self.snake[0] = (self.snake[0][0] - 60, self.snake[0][1])
 
     def mover_snake(self):
         for i in range(len(self.snake)-1, 0, -1):
@@ -52,7 +52,7 @@ class SNAKE:
         return False
     
     def colisao_cauda(self):
-        #print(self.snake[0],"==",corpo)
+        #print(self.snake[0],"==",self.snake[3:])
         if self.snake[0] in self.snake[3:]:
             del self.snake[0]
             return True
@@ -61,43 +61,55 @@ class SNAKE:
 
 class APPLE:
     def __init__(self):
-        self.x = (randint(120, 860) // 20) * 20
-        self.y = (randint(120, 680) // 20) * 20
+        self.bloco = 40
+        self.x = (randint(120, 860) // 60) * 60
+        self.y = (randint(120, 680) // 60) * 60
 
-    def spawnar_maca(self):
-        self.x = (randint(120, 860) // 20) * 20
-        self.y = (randint(120, 680) // 20) * 20
+    def spawnar_maca(self, corpo_cobra):
+        while True:
+            self.x = (randint(120, 860) // 60) * 60
+            self.y = (randint(120, 680) // 60) * 60
+            if (self.x, self.y) not in corpo_cobra:
+                break
 
     def colisao(self, pos_cobra):
         #print((self.x, self.y),"==",pos_cobra)
         return ((self.x, self.y) == pos_cobra)
 
+def vitoria():
+    fonte_mensagem = pygame.font.SysFont("Arial", 50)
+    mensagem1 = fonte_mensagem.render('GANHOU!', 1, (255, 255, 0))
+    tela.blit(mensagem1, (tela_largura/2 - 85, tela_altura/2 - 50))
+
+    mensagem2 = fonte_mensagem.render('JOGA  MUITO!', 1, (255, 255, 0))
+    tela.blit(mensagem2, (tela_largura/2 - 125, tela_altura/2))
 
 def mostrar_mensagem():
     fonte_mensagem = pygame.font.SysFont("Arial", 50)
-    mensagem = fonte_mensagem.render('APERTE "R" PARA REINICIAR', 1, (255, 255, 255))
+    mensagem = fonte_mensagem.render('APERTE "R" PARA REINICIAR', 1, (150, 0, 0))
     tela.blit(mensagem, (tela_largura/2 - imagem_bg.get_height()/2+35, tela_altura/2 - 50))
+    #print(imagem_bg.get_height() - 40, imagem_bg.get_width() - 40)
 
 def main():
-    COBRA = SNAKE([(360,360), (340,360), (320,360)], direita)
-    COBRA_skin = pygame.Surface((20, 20))
-    COBRA_skin.fill((0, 255, 0))
+    fps = 9
+    relogio = pygame.time.Clock()
+    rodando = True
+
+    COBRA = SNAKE([(360,360), (420,360), (480,360)], direita)
+    COBRA_skin = pygame.Surface((60, 60))
 
     MACA = APPLE()
-    MACA_skin = pygame.Surface((20, 20))
+    MACA_skin = pygame.Surface((60, 60))
     MACA_skin.fill((255, 0, 0))
 
     fonte_score = pygame.font.SysFont("Arial", 20)
     
-    relogio = pygame.time.Clock()
-    rodando = True
-
     while True:
-        relogio.tick(10)
-        #print(x_maca, y_maca)
+        relogio.tick(fps)
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
+                exit()
 
             if event.type == KEYDOWN:
                 if event.key == K_w or event.key == K_UP:
@@ -118,39 +130,47 @@ def main():
                     COBRA.anti_direcao = COBRA.direcao
                     COBRA.direcao = aux
                     
-
-                if event.key == K_r:
+                if event.key == K_r or event.key == K_BACKSPACE:
                     main()
-        
+
         if len(COBRA.snake) != 0:
-            if MACA.colisao(COBRA.snake[0]):
-                COBRA.pontos += 1
-                COBRA.snake.append((0, 0))
-                MACA.spawnar_maca()
+            if COBRA.pontos < 130:
+                if MACA.colisao(COBRA.snake[0]):
+                    COBRA.pontos += 1
+                    COBRA.snake.append((0, 0))
+                    if COBRA.pontos < 130:
+                        MACA.spawnar_maca(COBRA.snake)
+                    
+                COBRA.mover_snake()
+                COBRA.mudar_direcao()
 
-            COBRA.mover_snake()
-            COBRA.mudar_direcao()
+                if rodando:
+                    if COBRA.colisao_cauda():
+                        rodando = False
+                        del COBRA.snake[0]
 
-            if rodando:
-                if COBRA.colisao_cauda():
-                    rodando = False
+                    if COBRA.colisao_parede():
+                        rodando = False
+                        del COBRA.snake[0]
+
+                else:
                     del COBRA.snake[0]
-
-                if COBRA.colisao_parede():
-                    rodando = False
-                    del COBRA.snake[0]
-
-            else:
-                del COBRA.snake[0]
 
             tela.fill((0, 0, 0))
             tela.blit(imagem_bg, (99, 99))
             tela.blit(MACA_skin, (MACA.x, MACA.y))
 
-            if not rodando:
-                mostrar_mensagem()
+            if COBRA.pontos != 130:
+                if not rodando:
+                    mostrar_mensagem()
+            else:
+                vitoria()
 
-            for pos in COBRA.snake:
+            for i,pos in enumerate(COBRA.snake):
+                margem = (255 // 2) + 100
+                qntd = len(COBRA.snake)
+                tom_verde = 255 - (margem // qntd * i)
+                COBRA_skin.fill((0, tom_verde, 0))
                 tela.blit(COBRA_skin, pos)
             
         score = fonte_score.render(f"PONTUAÇÃO: {COBRA.pontos}", 1, (255, 255, 255))
